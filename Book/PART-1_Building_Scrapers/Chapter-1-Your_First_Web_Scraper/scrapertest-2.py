e6d219732b688b05d02a167588ecb6a81f49154f
Swiss <wankhedepawan1996@gmail.com>
2020-03-20 00:46:54 +0530

    initiated

---
+++ Book/PART-1_Building_Scrapers/Chapter-1-Your_First_Web_Scraper/scrapertest-2.py
@@ -0,0 +1,5 @@
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+html = urlopen("http://www.pythonscraping.com/exercises/exercise1.html")
+bsObj = BeautifulSoup(html.read(), features = "lxml");
+print(bsObj.h1)
